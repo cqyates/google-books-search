8e56ba2cde7b9f7c7bb866d54b96768083ecbbdf
Corey <cqyates@gmail.com>
2019-07-10 16:51:18 -0400

    create repo

---
+++ models/book.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+
+    googleId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    authors:{
+        type: [String],
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+    },
+    link: {
+        type: String,
+        required: true
+    }
+
+})
+
+const Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;
